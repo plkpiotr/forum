9e9931637fea89d73a602c2122ce79abc735cfe8
plkpiotr <piotrp10@o2.pl>
2017-09-16 21:23:50 +0200

    Move ForumApplication from forum to forum.configuration

--- src/main/java/com/plkpiotr/forum/configuration/ForumApplication.java
+++ src/main/java/com/plkpiotr/forum/configuration/ForumApplication.java
@@ -1,4 +1,4 @@
-package com.plkpiotr.forum;
+package com.plkpiotr.forum.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
