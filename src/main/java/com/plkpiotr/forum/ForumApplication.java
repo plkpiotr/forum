70ca626aa63c166ba253f44ce507999ea3ef1026
plkpiotr <piotrp10@o2.pl>
2017-09-17 12:25:16 +0200

    Test

--- src/main/java/com/plkpiotr/forum/ForumApplication.java
+++ src/main/java/com/plkpiotr/forum/ForumApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ForumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
+		// test
 	}
 }
