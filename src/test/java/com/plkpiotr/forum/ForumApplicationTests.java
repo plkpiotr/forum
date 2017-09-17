321fd25e3484ac975484286ecc870ce26f811e5b
plkpiotr <piotrp10@o2.pl>
2017-09-17 12:32:41 +0200

    Test of test

--- src/test/java/com/plkpiotr/forum/ForumApplicationTests.java
+++ src/test/java/com/plkpiotr/forum/ForumApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ForumApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        // text
+    }
 
 }
