2ad7e34da7832cf4a3daae9604dd88f3cc568d6a
plkpiotr <piotrp10@o2.pl>
2017-09-21 17:31:07 +0200

    Add AnswersController.java with two GetMapping, correct hyperlinks

---
+++ src/main/java/com/plkpiotr/forum/controller/AnswersController.java
@@ -0,0 +1,38 @@
+package com.plkpiotr.forum.controller;
+
+import com.plkpiotr.forum.entity.Answer;
+import com.plkpiotr.forum.repository.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class AnswersController {
+
+    private final AnswerRepository answerRepository;
+
+    @Autowired
+    public AnswersController(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
+    // TODO: add admin panel for all answers!
+
+    @GetMapping("answers/{id}")
+    public String displayAnswersByUser(@PathVariable String id, Model model) {
+        List<Answer> answers = answerRepository.findAnswerByUser_Id(Long.parseLong(id));
+        model.addAttribute("answers", answers);
+        return "answers";
+    }
+
+    @GetMapping("answers/useful/{id}")
+    public String displayUsefulAnswersByUser(@PathVariable String id, Model model) {
+        List<Answer> answers = answerRepository.findAnswerByUser_IdAndUseful(Long.parseLong(id), true);
+        model.addAttribute("answers", answers);
+        return "answers";
+    }
+}
--- src/main/java/com/plkpiotr/forum/repository/AnswerRepository.java
+++ src/main/java/com/plkpiotr/forum/repository/AnswerRepository.java
@@ -3,9 +3,13 @@ package com.plkpiotr.forum.repository;
 import com.plkpiotr.forum.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
     long countAnswersByUser_Id(long id);
     long countAnswersByUser_IdAndUseful(long user_id, boolean useful);
 
+    List<Answer> findAnswerByUser_Id(long id);
+    List<Answer> findAnswerByUser_IdAndUseful(long user_id, boolean useful);
 }
